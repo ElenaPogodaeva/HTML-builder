b99248cfb4fdb6f70e4d27d41320f7104198736e
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-11-05 19:30:30 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+const src = path.join(__dirname, 'files');
+const dest = path.join(__dirname, 'files-copy');
+
+async function copyDir(src, dest) {
+  try {
+    await fs.mkdir(dest, { recursive: true });
+    const files = await fs.readdir(src, {withFileTypes: true});
+    for (const item of files) {
+      if (item.isFile()) {
+        const srcPath = path.join(src, item.name);
+        const destPath =  path.join(dest, item.name);
+        await fs.copyFile(srcPath , destPath);
+      }
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+copyDir(src, dest);
