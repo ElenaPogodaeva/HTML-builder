89947e09d8d08fde60a17ab1c63ad2b5e59e0069
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-11-08 19:27:27 +0300

    feat: implement 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const fsp = require("fs/promises");
+const path = require("path");
+
+const srcDir = path.join(__dirname, "styles");
+const destDir = path.join(__dirname, "project-dist");
+
+async function mergeStyles(src, dest) {
+  const styleFile = path.join(dest, "bundle.css");
+
+  fs.writeFile(styleFile, "", (error) => {
+    if (error) return console.error(error.message);
+  });
+  try {
+    const files = await fsp.readdir(src, { withFileTypes: true });
+    for (const item of files) {
+      if (item.isFile() && path.extname(item.name) === ".css") {
+        const filePath = path.join(src, item.name);
+        const data = await readFile(filePath);
+
+        await fsp.writeFile(styleFile, data, { flag: "a" });
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function readFile(srcFile) {
+  const stream = fs.createReadStream(srcFile, "utf-8");
+  const result = [];
+  for await (const partData of stream) {
+    result.push(partData);
+  }
+  return result;
+}
+
+mergeStyles(srcDir, destDir);
